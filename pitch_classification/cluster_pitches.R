08061199f878724197754852e7e5c70386aff2f5
Jon Cusick <jonathan.cusick09@gmail.com>
2017-06-25 12:16:59 -0400

    Added initial plots

--- pitch_classification/cluster_pitches.R
+++ pitch_classification/cluster_pitches.R
@@ -16,25 +16,39 @@ pitches <-
 # Exploratory plotting -----------------------
 
 # Density of pitch type by speed
- ggplot(pitches, aes(start_speed)) + 
-    geom_density(aes(color = Pitch))
+speed <- ggplot(pitches, aes(start_speed)) + 
+    geom_density(aes(color = Pitch)) + 
+    labs(x = "Speed", y = "Density")
  
 # Pitch movement by type
- ggplot(pitches, aes(x = pfx_x, y = pfx_z)) +
-     geom_point(aes(color = Pitch))
+type <- ggplot(pitches, aes(x = pfx_x, y = pfx_z)) +
+    geom_point(aes(color = Pitch)) +
+    labs(x = "Horizontal Movement", y = "Vertical Movement")
  
- ggplot(pitches, aes(x = pfx_x, y = pfx_z)) + 
-     geom_point(aes(color = start_speed)) +
-     scale_color_gradient(low = "yellow", high = "red") + 
-     facet_grid(. ~ Pitch)
-
+# Speed and movement across pitch types
+facet <- ggplot(pitches, aes(x = pfx_x, y = pfx_z)) + 
+    geom_point(aes(color = start_speed)) +
+    scale_color_gradient(low = "yellow", high = "red") + 
+    facet_grid(. ~ Pitch) +
+    labs(x = "Horizontal Movement", y = "Vertical Movement",
+         color = "Speed")
+ggsave("./images/facet.png")
  
 # Cluster analysis --------------------------
- 
-clust1 <- Mclust(pitches[,c("start_speed", "break_y", "break_length",
-                             "break_angle")])
+p <- select(pitches, start_speed, break_y, break_length, break_angle,
+            pfx_x, pfx_z, Pitch, type_confidence) 
+clust <- Mclust(p[,c("start_speed", "break_y", "break_length", "break_angle")])
 #> Best model : ellipsoidal, equal shape (VEV) with 9 components 
  
 # Record BIC from EM algorithm
-clust1BIC <- mclustBIC(pitches[,c("start_speed", "break_y", "break_length",
-                             "break_angle")])
+clustBIC <- mclustBIC(p[, c("start_speed", "break_y", "break_length", "break_angle")])
+
+# Attach cluster membership/probability to each observation
+p$cclass <- clust$class
+cProb <- data.frame(clust$z)
+p <- bind_cols(p, cProb)
+
+# Compare cluster class to MLB class
+table(p$cclass, p$Pitch)
+ggplot(p, aes(x = pfx_x, y = pfx_z)) +
+    geom_point(aes(color = as.character(cclass)))
