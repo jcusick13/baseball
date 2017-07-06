7b916a396c1e61bb1e272fad94ddc2345dd70a14
Jon Cusick <jonathan.cusick09@gmail.com>
2017-07-05 21:59:29 -0400

    Normalize pitch type counts by cluster. Still not working all the way...

--- pitch_classification/cluster_pitches.R
+++ pitch_classification/cluster_pitches.R
@@ -1,6 +1,10 @@
 # cluster_pitches.R
 library(readr)
+library(tibble)
+library(tidyr)
+library(dplyr)
 library(mclust)
+library(ggplot2)
 
 # Read input data ----------------------------
 
@@ -23,7 +27,7 @@ speed <- ggplot(pitches, aes(start_speed)) +
 # Pitch movement by type
 type <- ggplot(pitches, aes(x = pfx_x, y = pfx_z)) +
     geom_point(aes(color = Pitch)) +
-    labs(x = "Horizontal Movement", y = "Vertical Movement")
+    labs(x = "Horizontal Movement", y = "Vertical Movement") 
  
 # Speed and movement across pitch types
 facet <- ggplot(pitches, aes(x = pfx_x, y = pfx_z)) + 
@@ -32,7 +36,6 @@ facet <- ggplot(pitches, aes(x = pfx_x, y = pfx_z)) +
     facet_grid(. ~ Pitch) +
     labs(x = "Horizontal Movement", y = "Vertical Movement",
          color = "Speed")
-ggsave("./images/facet.png")
  
 # Cluster analysis --------------------------
 p <- select(pitches, start_speed, break_y, break_length, break_angle,
@@ -48,7 +51,39 @@ p$cclass <- clust$class
 cProb <- data.frame(clust$z)
 p <- bind_cols(p, cProb)
 
-# Compare cluster class to MLB class
-table(p$cclass, p$Pitch)
+# Compare cluster class to MLB class, create normalized counts by cluster
+classct <- 
+    as.data.frame(table(p$cclass, p$Pitch)) %>%
+    rename(cluster = Var1, mlb = Var2, count = Freq) %>%
+    spread(mlb, count)
+
+# Create normalized matrix of cluster counts by row
+classct.norm <- data.frame(matrix(NA, nrow = nrow(classct), ncol = ncol(classct) - 1))
+for (i in 1:nrow(classct)) {
+    # Total pitches per cluster
+    total <- sum(classct[i,-1])
+    
+    for (j in 2:ncol(classct)) {
+        # Normalize values within each row
+        classct.norm[i, j - 1] <- classct[i, j] / total
+    }
+}
+
+# Combine with original class counts, reform for plotting
+n <- bind_cols(classct, classct.norm) %>%
+    gather(`Changeup`, `Curveball`, `Four-seam Fastball`, `Slider`, `Two-seam Fastball`,
+           key = "mlb", value = "count") %>%
+    gather(`X1`, `X2`, `X3`, `X4`, `X5`, key = normalized, value = "freq")
+
+    
+# Plot classification table   
+ggplot(n, aes(x = mlb, y = cluster)) +
+    geom_tile(aes(fill = count)) +
+    scale_fill_gradient(low = "red", high = "green")
+
+# Clusters by movement
 ggplot(p, aes(x = pfx_x, y = pfx_z)) +
     geom_point(aes(color = as.character(cclass)))
+
+
+
